4ed8080c149a250adc3a6e4ce636790310f1e5a3
victor82906 <molinaruizvictor82@gmail.com>
2026-01-03 18:47:40 +0100

    Primer commit

---
+++ src/main/java/com/vmr/cementerio/CementerioApplication.java
@@ -0,0 +1,13 @@
+package com.vmr.cementerio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CementerioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CementerioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmr/cementerio/CementerioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmr.cementerio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CementerioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
