38f8143c8cf00313f6c19ba236b5dab934855dba
victor82906 <molinaruizvictor82@gmail.com>
2026-02-13 16:52:25 +0100

    finalizado front

--- frontend/src/app/components/administrador/administrador.ts
+++ frontend/src/app/components/administrador/administrador.ts
@@ -4,15 +4,24 @@ import { Cabecera } from "../cabecera/cabecera";
 import { Footer } from '../footer/footer';
 import { RouterLink } from '@angular/router';
 import { AudioHome } from "../audio-home/audio-home";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-administrador',
-  imports: [Cabecera, Footer, RouterLink, AudioHome],
+  imports: [Cabecera, Footer, RouterLink, AudioHome, CommonModule, FormsModule],
   templateUrl: './administrador.html',
   styleUrl: './administrador.css',
 })
 export class Administrador {
 
-  constructor(private auth: AuthService){}
+  busqueda: string = '';
+
+  constructor(private auth: AuthService, private router: Router){}
+
+  buscar(){
+    this.router.navigate(['/busqueda-cementerio'], { queryParams: { nombre: this.busqueda } });
+  }
 
 }
